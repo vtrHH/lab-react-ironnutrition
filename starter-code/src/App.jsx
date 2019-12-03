a14e29c5deae6109bb01de6eaab6385234221600
José Carneiro <josemcarneiro@gmail.com>
2019-12-03 23:40:17 +0000

    Remove package-lock, references to Bulma, added Bootstrap, updated boilerplate

---
+++ starter-code/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+import foodList from './foods';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Sample App</p>
+      </div>
+    );
+  }
+}
+
+export default App;
